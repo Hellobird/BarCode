048cc19fa17b8dafcda7a377b4fde5ca43e7da02
Hellobird <472250569@qq.com>
2019-12-02 15:47:14 +0800

    增加本地图片解析类与相册图片解析demo

--- app/src/main/java/io/hellobird/barcodedemo/MainActivity.java
+++ app/src/main/java/io/hellobird/barcodedemo/MainActivity.java
@@ -1,10 +1,22 @@
 package io.hellobird.barcodedemo;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
@@ -12,10 +24,31 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.util.Arrays;
+
 import io.hellobird.barcode.CaptureActivity;
+import io.hellobird.barcode.decode.Decoder;
+import io.hellobird.barcode.decode.ImageResizer;
 import io.hellobird.barcode.encode.CodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_SCAN = 1;
+
+    private static final int REQUEST_PHOTO = 2;
+
+    /**
+     * 请求权限
+     */
+    public static final int REQUEST_READ_PERMISSION = 1234;
+
+    /**
+     * 二维码解析
+     */
+    private Decoder mQRDecoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +60,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
                 startActivityForResult(intent, 0);
+            }
+        });
+
+        findViewById(R.id.btn_photo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkReadPermission()) {
+                    accessStorage();
+                }
             }
         });
 
@@ -47,15 +89,120 @@ public class MainActivity extends AppCompatActivity {
                 imgCode.setImageBitmap(CodeEncoder.encodeQR(content, size));
             }
         });
+
+        mQRDecoder = new Decoder(BarcodeFormat.QR_CODE,BarcodeFormat.CODE_128);
     }
+
+    /**
+     * 检查读权限
+     *
+     * @return 读权限
+     */
+    private boolean checkReadPermission() {
+        int readPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        // 如果有权限
+        if (readPermission == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_PERMISSION);
+        return false;
+    }
+
+    /**
+     * 请求权限返回
+     *
+     * @param requestCode  请求码
+     * @param permissions  权限
+     * @param grantResults 结果
+     */
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_READ_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                accessStorage();
+            } else {
+                Toast.makeText(this, "请先同意访问相册权限", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    //访问文件相册信息
+    private void accessStorage() {
+        String acceptType = "image/*";
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType(acceptType);
+        startActivityForResult(intent, REQUEST_PHOTO);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && data != null) {
-            String result = data.getStringExtra(CaptureActivity.EXTRA_BARCODE);
-            TextView textView = findViewById(R.id.tv_result);
-            textView.setText("扫描结果为：" + result);
+        TextView textView = findViewById(R.id.tv_result);
+        if (requestCode == REQUEST_SCAN) {
+            if (resultCode == RESULT_OK && data != null) {
+                String result = data.getStringExtra(CaptureActivity.EXTRA_BARCODE);
+                textView.setText("扫描结果为：" + result);
+            }
+        } else if (requestCode == REQUEST_PHOTO) {
+            if (resultCode == RESULT_OK && data != null) {
+                Uri result = data.getData();
+                if (result != null) {
+                    String path = uri2filePath(result, this);
+                    Bitmap bitmap = ImageResizer.decodeSampledBitmapFromFile(path,500,500);
+                    ImageView imgCode = findViewById(R.id.img_code);
+                    imgCode.setImageBitmap(bitmap);
+                    Result codeResult = mQRDecoder.decodeBitmap(bitmap);
+                    if (codeResult == null) {
+                        textView.setText("解析图片失败");
+                        Toast.makeText(this, "解析图片失败", Toast.LENGTH_SHORT).show();
+                    } else {
+                        textView.setText("扫描结果为：" + codeResult.getText());
+                    }
+                }
+            }
         }
+    }
+
+    /**
+     * 将返回的uri转换为path
+     *
+     * @param uri     图片Uri
+     * @param context 上下文，用于访问contentProvider
+     * @return 图片文件地址
+     */
+    public static String uri2filePath(Uri uri, Context context) {
+        String path = "";
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT
+                || !DocumentsContract.isDocumentUri(context, uri)) {
+            String[] projection = {MediaStore.Images.Media.DATA};
+            Cursor cursor = context.getContentResolver().query(uri,
+                    projection, null, null, null);
+            if (cursor == null) {
+                return "";
+            }
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            path = cursor.getString(column_index);
+        }
+        // 版本4.4之后使用最近文件打开的图片处理方法有变化
+        else if (DocumentsContract.isDocumentUri(context, uri)) {
+            String wholeID = DocumentsContract.getDocumentId(uri);
+            String id = wholeID.split(":")[1];
+            String[] column = {MediaStore.Images.Media.DATA};
+            String sel = MediaStore.Images.Media._ID + "=?";
+            Cursor cursor = context.getContentResolver().query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, column, sel,
+                    new String[]{id}, null);
+            if (cursor == null) {
+                return "";
+            }
+            int columnIndex = cursor.getColumnIndex(column[0]);
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(columnIndex);
+            }
+            cursor.close();
+        }
+        return path;
     }
 }
---
+++ barcode/src/main/java/io/hellobird/barcode/decode/Decoder.java
@@ -0,0 +1,136 @@
+package io.hellobird.barcode.decode;
+
+import android.graphics.Bitmap;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.ReaderException;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EnumMap;
+import java.util.Map;
+
+/*******************************************************************
+ * Decoder.java  2019-11-29
+ * <P>
+ * 解析图片中的code<br/>
+ * <br/>
+ * </p>
+ *
+ * @author:zhoupeng
+ *
+ ******************************************************************/
+public class Decoder {
+    /**
+     * 解析类
+     */
+    public static String TAG = "Decoder";
+
+
+    /**
+     * 实际的解析类
+     */
+    private final MultiFormatReader mMultiFormatReader;
+
+    public Decoder(BarcodeFormat... formats) {
+        this(Arrays.asList(formats), null);
+    }
+
+    public Decoder(Collection<BarcodeFormat> decodeFormats, String charset) {
+        mMultiFormatReader = new MultiFormatReader();
+        Map<DecodeHintType, Object> hints = new EnumMap<>(DecodeHintType.class);
+        // 如果传入的支持格式为空，则默认添加
+        if (decodeFormats == null || decodeFormats.isEmpty()) {
+            throw new IllegalArgumentException("请传入指定的解析格式");
+        }
+        hints.put(DecodeHintType.POSSIBLE_FORMATS, decodeFormats);
+        if (charset != null) {
+            hints.put(DecodeHintType.CHARACTER_SET, charset);
+        }
+        mMultiFormatReader.setHints(hints);
+    }
+
+    /**
+     * 解析图像源中的码
+     *
+     * @param source 图像源
+     * @return 解析结果
+     */
+    public Result decode(LuminanceSource source) {
+        Result rawResult = null;
+        if (source != null) {
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            try {
+                rawResult = mMultiFormatReader.decodeWithState(bitmap);
+            } catch (ReaderException re) {
+                // continue
+            } finally {
+                mMultiFormatReader.reset();
+            }
+        }
+        return rawResult;
+    }
+
+    /**
+     * 解析bitmap中的码
+     *
+     * @param bitmap bitmap
+     * @return 解析结果
+     */
+    public Result decodeBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            Log.w(TAG, "解析失败，Bitmap 不能为空");
+            return null;
+        }
+        return decode(buildSourceFromBitmap(bitmap));
+    }
+
+    /**
+     * bitmap转换为zxing可识别的source
+     *
+     * @param bitmap bitmap
+     * @return source
+     */
+    private RGBLuminanceSource buildSourceFromBitmap(@NonNull Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int[] pixels = new int[width * height];
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+        return new RGBLuminanceSource(bitmap.getWidth(), bitmap.getHeight(), pixels);
+    }
+
+    /**
+     * 从文件中解析二维码
+     *
+     * @param filePath 图片Uri
+     * @param width    最大宽度，防止读取为bitmap后oom
+     * @param height   最大高度，防止读取为Bitmap后oom
+     * @return 解析结果
+     */
+    public Result decodeFile(String filePath, int width, int height) {
+        if (TextUtils.isEmpty(filePath)) {
+            Log.w(TAG, "解析失败，filePath 不能为空");
+            return null;
+        }
+
+        Bitmap bitmap = ImageResizer.decodeSampledBitmapFromFile(filePath, width, height);
+        if (bitmap != null) {
+            decodeBitmap(bitmap);
+        } else {
+            Log.w(TAG, "图片不能为空");
+        }
+        return null;
+    }
+
+}
